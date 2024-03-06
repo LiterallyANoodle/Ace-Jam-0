6f7cb88419150aeb3e3b7d7b542356360782b386
Matthew Mahan <themaskedkid321@gmail.com>
2024-03-06 17:28:10 -0600

    basic castle and environment

---
+++ Scenes/wall.gd
@@ -0,0 +1,14 @@
+class_name Wall
+extends Node3D
+
+@onready var grid_map : GridMap = $GridMap
+@onready var bricks_positions : Array[Vector3i] = grid_map.get_used_cells()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(bricks_positions)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
